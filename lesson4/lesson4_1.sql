16c9367d0d529f5d8fd7bd68645f335e499898b0
Rail <railbolo@gmail.com>
2022-10-03 23:33:49 +0400

    new

---
+++ lesson4/lesson4_1.sql
@@ -0,0 +1,62 @@
+USE lesson4;
+
+create table users
+(
+    id  int auto_increment primary key,
+    login varchar(255) null,
+    pass  varchar(255) null,
+    male  tinyint      null
+);
+
+create table clients
+(
+    id    int auto_increment primary key,
+    login varchar(255) null,
+    pass  varchar(255) null,
+    male  tinyint      null
+);
+
+INSERT INTO users (login, pass, male) VALUES ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO users (login, pass, male) VALUES ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO users (login, pass, male) VALUES ('Olia', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2);
+INSERT INTO users (login, pass, male) VALUES ('Tom', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1);
+INSERT INTO users (login, pass, male) VALUES ('Margaret', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
+INSERT INTO users (login, pass, male) VALUES ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+
+INSERT INTO clients (login, pass, male) VALUES ('alexander', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Olia', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2);
+INSERT INTO clients (login, pass, male) VALUES ('Dmitry', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Margaret', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
+INSERT INTO clients (login, pass, male) VALUES ('Leonid', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Mikle', '$ws$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Olga', '$2y$10$88zbBCKLJklStIgTqBKIluijJUnbeZ5WqJI4RJgkksnFZon5kH14y', 2);
+INSERT INTO clients (login, pass, male) VALUES ('Tom', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH20y', 1);
+INSERT INTO clients (login, pass, male) VALUES ('Masha', '$2y$20$6SzbBCNRNklStIgTqBKIluijJUnbeZ4wqIu4RJgkksnFZon5kH20y', 2);
+INSERT INTO clients (login, pass, male) VALUES ('alex', '$2y$10$6SzbBCMENklStIgTqBKIluijJUnbeZ5WqIu4RJgkksnFZon5kH14y', 1);
+
+
+
+
+-- Задание:
+-- 1.	Получить список пользователей и клиентов, удалив одинаковых клиентов и пользователей
+-- 2.	Получить список пользователей и клиентов. Дубликаты удалять не нужно
+-- 3.   Получить столбцы из таблицы “clients” , в которых первое имя является набором значений.
+-- 4.   Выберите все логины из таблицы “users”, кроме “Mikle”.
+
+
+-- 1 ----
+SELECT * FROM users
+UNION
+SELECT * FROM clients;
+-- 2 ----
+SELECT * FROM users
+UNION ALL
+SELECT * FROM clients;
+-- 3 ----
+SELECT * FROM clients
+WHERE login IN ('Mikle', 'Tom');
+-- 4 ----
+SELECT * FROM clients
+-- WHERE 'Mikle' NOT IN (login);
+WHERE login NOT IN ('Mikle'); -- АНАЛОГИЧНО
